8a334610ddbaa212dbbd602c2938a1c813d38ec5
Kevin Renault <kevinrenaultoff@gmail.com>
2022-11-01 16:46:49 +0100

    bloque l'accès à page accueil si pas connecté + message erreur

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventsController extends AbstractController
 {
     #[Route('/accueil', name: 'homepage')]
-    public function index(): Response
+    public function accueil(Request $request): Response
     {
+
+        $request->getSession()->getFlashBag()->add('note', 'Vous devez être connecté pour accéder au site');
+
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
 
         return $this->render('events/homepage.html.twig', [
