246f86355fbc654acdc34c850ef1be501243732f
Kevin RENAULT <99198628+KevRWB@users.noreply.github.com>
2022-11-07 15:59:36 +0100

    fix event.twig

---
+++ src/Form/CreateLocationType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Location;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateLocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('street')
+            ->add('latitude')
+            ->add('longitude')
+//            ->add('locationPicture')
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'label' => 'Ville',
+//                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
